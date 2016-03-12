746fe8ade83a43596e9302067bdc5bf77d6db4d0
w.santos <w.santos.python@gmail.com>
2016-03-12 20:32:12 -0300

    criando estrutara basica para rodar o flask

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+# Criando instacia principal do Flask
+app = Flask(__name__)
+
+# Rota para pagina inicial (home)
+app.route('/')
+def home():
+    return 'hello world -- Academic forge'
+
+if __name__ == '__main__':
+    app.run()
