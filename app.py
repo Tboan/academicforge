6e276746b1fd4fb89aa10c27404bf9a13a58f993
w.santos <w.santos.python@gmail.com>
2016-03-12 22:21:29 -0300

    obtendo config apartir do envoriment

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
+
 from flask import Flask
 
 # Criando instacia principal do Flask
 app = Flask(__name__)
 
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 # Rota para pagina inicial (home)
 @app.route('/')
 def home():
-    return 'hello world -- Academic forge'
+    return 'hello world -- Academic forge - {}'.format(os.environ['APP_SETTINGS'])
 
 
 if __name__ == '__main__':
